3d287614add7f44c09ed3e33fec9afa497065653
itsbot <patrickiannini@knights.ucf.edu>
2021-09-10 18:52:16 -0400

    Completed Exercise

--- src/main/java/Exercise11/Exercise11.java
+++ src/main/java/Exercise11/Exercise11.java
@@ -3,9 +3,24 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise11;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many euros are you exchanging?");
+        int euros = sc.nextInt();
+
+        System.out.println("What is the exchange rate?");
+        double eRate = sc.nextDouble();
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        double dollars = (euros * eRate);
+
+        System.out.println(euros+" euros at an exchange rate of "+eRate+" is "+df.format(dollars)+" U.S. dollars.");
     }
 }
